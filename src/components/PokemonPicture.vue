c59781f72f21342dd12e8922c710a7cab561e1df
higueraDev <davidhigueralp@hotmail.com>
2022-05-06 21:33:55 -0600

    [1.0][UPDATED] Components Styles

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -3,7 +3,37 @@ export default {};
 </script>
 
 <template>
-	<h1>Pokemon Picture</h1>
+	<div class="pokemon-container">
+		<img
+			class="hidden-pokemon"
+			src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+			alt="pokemón"
+		/>
+		<!-- <img
+			class="fade-in"
+			src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+			alt="pokemón"
+		/> -->
+	</div>
 </template>
 
-<style></style>
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+	height: 200px;
+}
+img {
+	height: 200px;
+	position: absolute;
+	width: 100vw;
+	margin-left: -50vw;
+	user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	-webkit-user-drag: none;
+	-webkit-user-select: none;
+}
+.hidden-pokemon {
+	filter: brightness(0);
+}
+</style>
