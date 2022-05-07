70d020787e00bfbcb6dfb417323e47414dde9a68
higueraDev <davidhigueralp@hotmail.com>
2022-05-06 23:48:55 -0600

    [1.0][ADDED]Helper getPokemonOptions - API pokemonApi

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,14 +1,18 @@
 <script>
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import PokemonOptions from "@/components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+console.log(getPokemonOptions());
+
 export default {
-    components: { PokemonPicture, PokemonOptions }
+	components: { PokemonPicture, PokemonOptions },
 };
 </script>
 
 <template>
 	<h1>¿Quien es este pokemón?</h1>
-    <PokemonPicture :showPokemon="false" :pokemonId="10"></PokemonPicture>
-    <PokemonOptions></PokemonOptions>
+	<PokemonPicture :showPokemon="false" :pokemonId="10"></PokemonPicture>
+	<PokemonOptions></PokemonOptions>
 </template>
 <style></style>
