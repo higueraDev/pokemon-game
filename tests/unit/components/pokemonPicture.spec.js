13a4a17efeefae207ac1e2d6f7bb6b5697acf161
higueraDev <davidhigueralp@hotmail.com>
2022-05-07 14:11:57 -0600

    1.0[ADDED]test:pokemonPicture

---
+++ tests/unit/components/pokemonPicture.spec.js
@@ -0,0 +1,42 @@
+import { shallowMount } from "@vue/test-utils";
+
+import PokemonPicture from "@/components/PokemonPicture";
+
+describe("PokemonPicture component", () => {
+	test("should match snapshot", () => {
+		const wrapper = shallowMount(PokemonPicture, {
+			props: {
+				pokemonId: 1,
+				showPokemon: false,
+			},
+		});
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+	test("should show hidden-image and pokemon 100", () => {
+		const wrapper = shallowMount(PokemonPicture, {
+			props: {
+				pokemonId: 100,
+				showPokemon: false,
+			},
+		});
+
+		const [img1,img2] = wrapper.findAll("img");
+		expect(img1.exists()).toBeTruthy();
+		expect(img2).toBe(undefined);
+
+		expect(img1.classes("hidden-pokemon")).toBe(true);
+        expect(img1.attributes("src")).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg")
+	});
+	test("should show pokemon if showpokemon is true", () => {
+        const wrapper = shallowMount(PokemonPicture, {
+            props: {
+                pokemonId: 100,
+				showPokemon: true,
+			},
+		});
+        const img = wrapper.find("img");
+        expect(img.exists()).toBeTruthy();
+        expect(img.classes("fade-in")).toBe(true);
+
+    });
+});
